5572696709efca4a195d2b2846d860e57a87e557
MicMun <michael.munzert@micmun.de>
2022-04-19 17:36:08 +0200

    Sort names case insensitive

--- app/src/main/java/de/micmun/android/nextcloudcookbook/util/NameComparator.kt
+++ app/src/main/java/de/micmun/android/nextcloudcookbook/util/NameComparator.kt
@@ -11,11 +11,12 @@ import de.micmun.android.nextcloudcookbook.db.model.DbRecipe
  * Comparator with name.
  *
  * @author MicMun
- * @version 1.1, 21.03.21
+ * @version 1.2, 19.04.2022
  */
 class NameComparator(private val asc: Boolean) : Comparator<DbRecipe> {
    override fun compare(o1: DbRecipe, o2: DbRecipe): Int {
-      return if (asc) o1.recipeCore.name.compareTo(o2.recipeCore.name) else o2.recipeCore.name.compareTo(
-         o1.recipeCore.name)
+      val name1 = o1.recipeCore.name.lowercase()
+      val name2 = o2.recipeCore.name.lowercase()
+      return if (asc) name1.compareTo(name2) else name2.compareTo(name1)
    }
 }
